86572b1d666c3fc5fca80c17a08f5ab2919fa198
CtrlPlay123 <160868378+CtrlPlay123@users.noreply.github.com>
2024-06-19 18:54:09 -0300

    Update3
    Neste Update adicionamos o sitema de mandat mensagem pelo whats app no formulário, porém ainda está fatando o script para enviar as informações do fomulário

---
+++ Assets/JS/script.js
@@ -0,0 +1,19 @@
+	<script>
+  document.querySelector('.btn_comprar').addEventListener('click', function() {
+    const name = document.getElementById('name').value;
+    const lancheName = document.getElementById('lancheName').value;
+    const address = document.getElementById('address').value;
+    const tel = document.getElementById('tel').value;
+    const payment = document.getElementById('payment').value;
+
+    const mensagem = `Pedido de compra:%0A
+      Nome do cliente: ${name}%0A
+      Lanche escolhido: ${lancheName}%0A
+      Endereço de entrega: ${address}%0A
+      Telefone de contato: ${tel}%0A
+      Forma de pagamento: ${payment}`;
+
+    const whatsappLink = `https://api.whatsapp.com/send?phone=+551239523334&text=${mensagem}`;
+    window.open(whatsappLink, '_blank');
+  });
+</script>
